8ac1db65016298301b12fb2df91b8e38d36c6f4f
Stamich47 <159186195+Stamich47@users.noreply.github.com>
2024-06-18 13:31:29 -0700

    adjust grid & add erase/reset mode

--- script.js
+++ script.js
@@ -1,8 +1,46 @@
 const container = document.querySelector('.container');
 
-for (let i = 0; i < 256; i++) {
+alert(
+  `Move the mouse over the grid to draw \n \n Left mouse click anywhere in the grid to toggle Erase Mode \n \n Press the Spacebar to reset the grid`
+);
+
+for (let i = 0; i < 2500; i++) {
   const div = document.createElement('div');
+  div.classList.add('no-hover');
+  div.style.cssText = 'flex-basis: 2%';
   container.appendChild(div);
 }
 
-console.log(container);
+const squares = document.querySelectorAll('.no-hover');
+
+function toggleSquare() {
+  squares.forEach((square) => {
+    square.addEventListener('mouseover', () => {
+      if (square.classList.contains('hover')) {
+        square.classList.remove('hover');
+      } else {
+        square.classList.add('hover');
+      }
+    });
+  });
+}
+
+squares.forEach((square) => {
+  square.addEventListener('mouseover', () => {
+    square.classList.add('hover');
+    square.addEventListener('click', toggleSquare);
+  });
+});
+
+function eraseBoard() {
+  document.addEventListener('keydown', (e) => {
+    e.preventDefault();
+    if (e.code === 'Space') {
+      squares.forEach((square) => {
+        square.classList.remove('hover');
+      });
+    }
+  });
+}
+
+eraseBoard();
